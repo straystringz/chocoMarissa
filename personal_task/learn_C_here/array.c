f22d13316097189b2bfb1181875e119353d6ef5a
Victor <victor.j.emmanuel@hotmail.com>
2022-10-08 12:26:30 +0000

    I added a for_loop with similar solution to the code here

--- personal_task/learn_C_here/array.c
+++ personal_task/learn_C_here/array.c
@@ -11,8 +11,8 @@ int main(void)
 
     //getting user's scores
     do{
-    printf("Enter score: ");
-    scanf("%d", &score[i]); //
+    printf("\nEnter score: ");
+    scanf("%d", &score[i]);
     i++;
     }
     while(i<3);
@@ -20,4 +20,14 @@ int main(void)
         printf("\nYour average is %f", (score[0] + score[1] + score[2]) / (float) 3);
     }
     
+    //I want to try it with for loop.
+    
+    score[3];
+    
+    for (i = 0; i < 3; i++)
+    {
+     printf("\nEnter score: ");
+    scanf("%d", &score[i]);
+    }
+    printf("\nYour average is %f", (score[0] + score[1] + score[2]) / (float) 3);
 }
