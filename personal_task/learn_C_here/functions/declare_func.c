ccdb1bf53bc404dfe58c1c67edb286d0064244ab
Victor <victor.j.emmanuel@hotmail.com>
2022-10-10 16:04:43 +0000

    Functions help us solve the repetition problem

---
+++ personal_task/learn_C_here/functions/declare_func.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//Function declaration (prototyping);
+
+/** 
+ * This is a process of declaring the properties of a function.
+ * 
+ * this is a good way you can hint the compiler to expect something (a function ) other than "main".
+ * 
+ * Syntax return_type func_name (input);
+ * the input can hold different types of parrameters(int, char, ...) "it's not compulsory to add input parameter names, just their types are enough"
+ * 
+ * notice the semi-colon at the end... this differenciates a prototype from the function itself(Func prototype must end with a semi-colon);
+ * 
+ */
+ 
+ 
+ char fun(); // Function declaration/prototyping
+ 
+int main()
+{
+    char c = fun();//from here the controle skips to look for the function called(in this case, 'fun')
+    printf("Character is: %c\n", c);
+}
+
+char fun()//note that the function must match wit the prototype format.
+{
+    return 'a';
+}
